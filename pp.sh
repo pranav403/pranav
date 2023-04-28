bb866526d2ba6a8f7ef567d727975216a177b88e
pranav403 <114681896+pranav403@users.noreply.github.com>
2023-04-28 15:21:17 +0530

    Update pp.sh

--- pp.sh
+++ pp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /tmp/docker-demo/
-docker build -t my-image .
-docker run -d --name my-container -p 80:80 my-image
+sudo docker build -t my-image .
+sudo docker run -d --name my-container -p 80:80 my-image
